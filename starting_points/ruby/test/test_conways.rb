a4e28afd9ab9ac37d099c3d4f5951a57f1eac282
Kevin R. Barnes <vinbarnes@gmail.com>
2011-12-03 13:02:03 -0600

    better starting point

--- starting_points/ruby/test/test_conways.rb
+++ starting_points/ruby/test/test_conways.rb
@@ -1,14 +1,7 @@
-#!/usr/bin/env ruby
-
 require 'test/unit'
-require_relative 'test_helper'
-require 'conways'
 
-include Test::Unit
-
-class ConwaysTest < TestCase
+class ConwaysTest < include Test::Unit::TestCase
   def test
     assert(true)
   end
 end
-  
